bc33c8c6f91c182097c2a118b17effb453dab556
poor0-kaws <swaroop.hebbar7@gmail.com>
2026-03-03 16:17:10 -0500

    Refactor imports and add initial planning comments for simulation steps

--- sim.py
+++ sim.py
@@ -1,4 +1,10 @@
-import pybullet
+import pybullet as p 
 import pybullet_data
 import numpy as np 
-import time 
+import time
+
+# Plan 
+# 1. Load the plane, panda, and 2 cubes, set the gravity, set the cubes's friction and positions, open up the gripper
+# 2. Design get_cube, open_gripper, close_gripper, stack()
+# 3. Panda opens its gripper, gets the first cube and places it in a place, then it gets teh second cubes and places it on the first cube
+# 4. 
